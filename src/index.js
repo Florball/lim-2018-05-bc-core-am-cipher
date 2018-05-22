e8209431f7fdcbf1626cc0c6fda1749807b4638d
Florball <flor_ballinas@hotmail.com>
2018-05-22 10:27:08 -0500

    cambió en las palabras reservadas var por let

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // Pide nombre a usuario y devuelve el nombre con la inicial mayuscula.
-var name = prompt('¡Hola! \n ¿Cuál es tu nombre?'); 
+let name = prompt('¡Hola! \n ¿Cuál es tu nombre?'); 
 name = name.charAt(0).toUpperCase() + name.slice(1);
 
 // Muestra un saludo en pantalla con el nombre del usuario.
-var welcomeName = document.getElementById('welcome');
+let welcomeName = document.getElementById('welcome');
 welcomeName.innerHTML = '¡Bienvenido(a) ' + name + '!';
 
