abc65c2381f48d4337d3c5838dc92106fad39241
Florball <flor_ballinas@hotmail.com>
2018-05-29 10:20:14 -0500

    eliminando saludo de bienvenida

--- src/index.js
+++
@@ -1,8 +0,0 @@
-// Pide nombre a usuario y devuelve el nombre con la inicial mayuscula.
-let name = 
-name = name.charAt(0).toUpperCase() + name.slice(1);
-
-// Muestra un saludo en pantalla con el nombre del usuario.
-let welcomeName = document.getElementById('welcome');
-welcomeName.innerHTML = '¡Bienvenido(a) ' + name + '!';
-
