352808092f603b9425fb59192215c1f8c8ca44ba
Florball <flor_ballinas@hotmail.com>
2018-05-30 00:36:47 -0500

    haciendo responsive h1 e imagen de cifrado cesar

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,10 @@
-window.cipher = {
-  // ... 
-};
+// window.cipher = {
+//   encode: (offset, string) => {
+//     let encryptedWord = '';
+//   //   for (let index = 0; index <= string.length; index++) {
+       
+//   // },
+//   // decode: (offset, string) => {
+
+//   // }
+// }
