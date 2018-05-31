7e8dee02053a5b0157370dda73b8be28909ebe9d
Florball <flor_ballinas@hotmail.com>
2018-05-31 03:45:06 -0500

    cifrado y descifrado aun no reconoce espacios

--- src/cipher.js
+++ src/cipher.js
@@ -6,14 +6,22 @@ window.cipher = {
     for (let index = 0; index < string.length; index++) {
       position = string.charCodeAt(index);
       if ((position >= 65 && position <= 90) || (position >= 97 && position <= 122)) {
-        encryption = (position - 90 + offset) % 26 + 90 || (position - 122 + offset) % 26 + 122;
-        encryption += String.fromCharCode(position);
-
-      }
-      decode: (offset, string) => {
-        console.log('chau')
+        let letter = (position - 90 + offset) % 26 + 90 || (position - 122 + offset) % 26 + 122;
+        encryption += String.fromCharCode(letter);
       }
     }
-    return(encryption);
-  } 
+    return (encryption);
+  },
+  decode: (offset, string) => {
+    let decrypted = '';
+    let position = '';
+    for (let index = 0; index < string.length; index++) {
+      position = string.charCodeAt(index);
+      if ((position >= 65 && position <= 90) || (position >= 97 && position <= 122)) {
+        let letter = (position - 90 - offset) % 26 + 90 || (position - 122 - offset) % 26 + 122;
+        decrypted += String.fromCharCode(letter);
+      }
+    }
+    return(decrypted);
+  }
 }
