6d034231035a30f7bc86d438cfbf364a2de2f328
Florball <flor_ballinas@hotmail.com>
2018-05-31 00:19:01 -0500

    creando contantes y funciones para el cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,10 +1,11 @@
-// window.cipher = {
-//   encode: (offset, string) => {
-//     let encryptedWord = '';
-//   //   for (let index = 0; index <= string.length; index++) {
-       
-//   // },
-//   // decode: (offset, string) => {
+window.cipher = {
+  encode: (offset, string) => {
+    
+    for (let index = 0; index <= string.length; index++) {
 
-//   // }
-// }
+    },
+    decode: (offset, string) => {
+
+    }
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const displace = document.getElementById('displace'); // Cuadro para elegir desplazamiento de cifrado.
+const cipherString = document.getElementById('cipher'); // Cuadro de texto para ingresar frase a cifrar.
+const buttonEncode = document.getElementById('buttonEncode'); // Boton para cifrar.
+const encyptedText = document.getElementById('encryptedText'); // Texto cifrado.
+const decipherString = document.getElementById('decipher'); // Cuadro de texto para descifrar.
+const buttonDecode = document.getElementById('buttonDecode'); // Boton para descifrar.
+const decryptedText = document.getElementById('decryptedText'); // Texto descifrado.
+
+buttonEncode.addEventListener('click', ()=>{
+  encryptedText.innerHTML = cipher.encode(parseInt(displace.value), cipherString.value);
+});
+buttonDecode.addEventListener('click', ()=>{
+  decryptedText.innerHTML = cipher.encode(parseInt(displace.value), decipherString.value);
+});
