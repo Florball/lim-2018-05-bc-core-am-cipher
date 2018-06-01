15032f8cfcb3520d7e9ba4c235b88b23f1abf810
Florball <flor_ballinas@hotmail.com>
2018-05-31 21:21:29 -0500

    edicion de test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const offset = document.getElementById('displace'); // Cuadro para elegir desplazamiento de cifrado.
 const cipherString = document.getElementById('cipher'); // Cuadro de texto para ingresar frase a cifrar.
 const buttonEncode = document.getElementById('buttonEncode'); // Boton para cifrar.
-const encyptedText = document.getElementById('encryptedText'); // Texto cifrado.
+const encryptedText = document.getElementById('encryptedText'); // Texto cifrado.
 const decipherString = document.getElementById('decipher'); // Cuadro de texto para descifrar.
 const buttonDecode = document.getElementById('buttonDecode'); // Boton para descifrar.
 const decryptedText = document.getElementById('decryptedText'); // Texto descifrado.
--- test/cipher.spec.js
+++ test/cipher.spec.js
@@ -11,8 +11,7 @@ describe('cipher', () => {
     });
 
     it('debería retornar "HIJKLMNOPQRSTUVWXYZABCDEFG" para "ABCDEFGHIJKLMNOPQRSTUVWXYZ" con offest 33', () => {
-      const result = cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');
-      assert.equal(result, 'HIJKLMNOPQRSTUVWXYZABCDEFG');
+      assert.equal(cipher.encode(33, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'), 'HIJKLMNOPQRSTUVWXYZABCDEFG');
     });
   });
 
@@ -22,7 +21,9 @@ describe('cipher', () => {
       assert.equal(typeof cipher.decode, 'function');
     });
 
-    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33');
+    it('debería retornar "ABCDEFGHIJKLMNOPQRSTUVWXYZ" para "HIJKLMNOPQRSTUVWXYZABCDEFG" con offest 33', () => {
+      assert.equal(cipher.decode(33, "HIJKLMNOPQRSTUVWXYZABCDEFG"), "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    });
   });
 
   describe('cipher.createCipherWithOffset', () => {
