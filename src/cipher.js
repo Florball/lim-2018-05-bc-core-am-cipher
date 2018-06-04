776c148a0c2e5c244bc9e69a3bc8bcc062300d6a
Florball <flor_ballinas@hotmail.com>
2018-06-03 23:57:24 -0500

    guardando cometarios en los cudtro documentos y guardando nuevos estilos de color en css

--- src/cipher.js
+++ src/cipher.js
@@ -1,29 +1,29 @@
+// Objeto con funciones para cifrar y descifrar
 window.cipher = {
-  encode: (offset, string) => {
-    // console.log('hola');
-    let encryption = '';
-    for (let index = 0; index < string.length; index++) {
-      let position = string.charCodeAt(index);
-      if (position >= 65 && position <= 90) {
-        let value = (position - 65 + offset) % 26 + 65;
-        encryption += String.fromCharCode(value);
-      } else if (position == 32) {
-        encryption += " ";
+  encode: (offset, string) => { // Funcion para cifrar
+    let encryption = ''; //Declarando un acumulador para el texto  
+    for (let index = 0; index < string.length; index++) { // Ciclo repetitivo para recorrer el texto a cifrar
+      let position = string.charCodeAt(index); // Declarando una variable que guardará el codigo Ascii
+      if (position >= 65 && position <= 90) { // Ciclo condicional para que reconozca las mayusculas
+        let value = (position - 65 + offset) % 26 + 65; // Variable con la formula del cifrado Cesar
+        encryption += String.fromCharCode(value); // Variable reasignada, retornandole el valor de letra.
+      } else if (position == 32) { // Condicional, si es igual a 32 en codigo ascii
+        encryption += " "; // Es igual a espacio
       }
     }
-    return (encryption);
+    return (encryption); // Retornando el valor
   },
-  decode: (offset, string) => {
-    let decrypted = '';
-    for (let index = 0; index < string.length; index++) {
-      let position = string.charCodeAt(index);
-      if (position >= 65 && position <= 90) {
-        let value = (position - 90 - offset) % 26 + 90;
-        decrypted += String.fromCharCode(value);
-      } else if (position == 32) {
-        decrypted += " ";
+  decode: (offset, string) => { // Funcion para deecifrar.
+    let decrypted = ''; // //Declarando un acumulador para el texto
+    for (let index = 0; index < string.length; index++) { // Ciclo repetitivo para recorrer el texto a decifrar
+      let position = string.charCodeAt(index); // Declarando una variable que guardará el codigo Ascii
+      if (position >= 65 && position <= 90) { // Ciclo condicional para que reconozca las mayusculas
+        let value = (position - 90 - offset) % 26 + 90; // Variable con la formula del cifrado Cesar
+        decrypted += String.fromCharCode(value); // Variable reasignada, retornandole el valor de letra.
+      } else if (position == 32) { // Condicional, si es igual a 32 en codigo ascii
+        decrypted += " "; // Es igual a espacio
       }
     }
-    return (decrypted);
+    return (decrypted); // Retornando el valor
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
+// Accediendo al DOM 
 const offset = document.getElementById('displace'); // Cuadro para elegir desplazamiento de cifrado.
+const offset2 = document.getElementById('displace2'); // Segundo cuadro para elegir desplazamiento de cifrado.
 const cipherString = document.getElementById('cipher'); // Cuadro de texto para ingresar frase a cifrar.
 const buttonEncode = document.getElementById('buttonEncode'); // Boton para cifrar.
 const encryptedText = document.getElementById('encryptedText'); // Texto cifrado.
@@ -6,9 +8,10 @@ const decipherString = document.getElementById('decipher'); // Cuadro de texto p
 const buttonDecode = document.getElementById('buttonDecode'); // Boton para descifrar.
 const decryptedText = document.getElementById('decryptedText'); // Texto descifrado.
 
-buttonEncode.addEventListener('click', ()=>{
+// Llamando al evento, para boton cifrar y descifrar.
+buttonEncode.addEventListener('click', () => {
   encryptedText.innerHTML = cipher.encode(parseInt(offset.value), cipherString.value.toUpperCase());
 });
-buttonDecode.addEventListener('click', ()=>{
-  decryptedText.innerHTML = cipher.decode(parseInt(offset.value), decipherString.value.toUpperCase());
+buttonDecode.addEventListener('click', () => {
+  decryptedText.innerHTML = cipher.decode(parseInt(offset2.value), decipherString.value.toUpperCase());
 });
