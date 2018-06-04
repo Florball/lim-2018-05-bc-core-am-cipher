c3e04804157fa5b2d66d38728b88dab359e3f07e
Florball <flor_ballinas@hotmail.com>
2018-06-04 02:45:20 -0500

    añadiendo funcion para añadir tabsen el navegador

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// Accediendo al DOM 
+// Accediendo al DOM para el cifrado y decifrado.
 const offset = document.getElementById('displace'); // Cuadro para elegir desplazamiento de cifrado.
 const offset2 = document.getElementById('displace2'); // Segundo cuadro para elegir desplazamiento de cifrado.
 const cipherString = document.getElementById('cipher'); // Cuadro de texto para ingresar frase a cifrar.
@@ -15,3 +15,26 @@ buttonEncode.addEventListener('click', () => {
 buttonDecode.addEventListener('click', () => {
   decryptedText.innerHTML = cipher.decode(parseInt(offset2.value), decipherString.value.toUpperCase());
 });
+
+// Accediendo al DOM y añadiendo una funcion para usar Tabs.
+document.getElementById('cipherCesar').addEventListener('click', () => {
+  document.getElementById('module1').style.display = 'block';
+  document.getElementById('module2').style.display = 'none';
+  document.getElementById('module3').style.display = 'none';
+
+});
+
+document.getElementById('code').addEventListener('click', () => {
+  document.getElementById('module2').style.display = 'block';
+  document.getElementById('module1').style.display = 'none';
+  document.getElementById('module3').style.display = 'none';
+
+});
+
+document.getElementById('decode').addEventListener('click', () => {
+  document.getElementById('module3').style.display = 'block';
+  document.getElementById('module1').style.display = 'none';
+  document.getElementById('module2').style.display = 'none';
+
+});
+
